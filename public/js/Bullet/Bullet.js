1c517d0efa4040b680b699729bfef88be8521232
Oktay <24858380+aoktayd@users.noreply.github.com>
2018-09-19 21:10:12 +0200

    change constructor functions to classes

--- public/js/Bullet/Bullet.js
+++ public/js/Bullet/Bullet.js
@@ -1,26 +1,27 @@
-function Bullet(x, y, playerAngle, shooterId, id) {
-  this.pos = createVector(x, y);
-  this.velocity = p5.Vector.fromAngle(playerAngle);
-  this.velocity.mult(10);
-  this.r = 10;
-  this.shooterId = shooterId;
-  this.id = id;
+class Bullet{
+  constructor(x, y, playerAngle, shooterId, id) {
+    this.pos = createVector(x, y);
+    this.velocity = p5.Vector.fromAngle(playerAngle);
+    this.velocity.mult(10);
+    this.r = 10;
+    this.shooterId = shooterId;
+    this.id = id;
+  }
 
-
-  this.updateAndDisplay = function() {
+  updateAndDisplay() {
     this.r = lerp(this.r, 0, 0.005);
     this.pos.add(this.velocity);
     fill(255);
     ellipse(this.pos.x, this.pos.y, this.r, this.r);
   }
 
-  this.hasBulletDiminished = function() {
+  hasBulletDiminished() {
     if (this.hasDiminished()) {
       return true;
     }
   }
 
-  this.checkCollisionsWithPlayer = function(bullets, player, i) {
+  checkCollisionsWithPlayer(bullets, player, i) {
     // console.log(socket.id);
     // console.log(bullets);
     // console.log("Bullet clientId: " +bullets.clientId);
@@ -33,20 +34,18 @@ function Bullet(x, y, playerAngle, shooterId, id) {
 
   }
 
-  this.hasHit = function(asteroid) {
+  hasHit(asteroid) {
     return dist(this.pos.x, this.pos.y, asteroid.pos.x, asteroid.pos.y) <= asteroid.r;
   }
 
-  this.hasHitPlayer = function(player) {
+  hasHitPlayer(player) {
     return dist(player.pos.x, player.pos.y, this.pos.x, this.pos.y) <= player.r;
   }
 
-  this.hasDiminished = function() {
+  hasDiminished() {
     if (this.r <= 1) {
       return true;
     }
     return false;
   }
-
-
 }
--- public/js/Food/Food.js
+++ public/js/Food/Food.js
@@ -1,15 +1,14 @@
+class Food {
+  constructor(x, y, r, id) {
+    this.x = x;
+    this.y = y;
+    this.r = r;
+    this.id = id;
+  }
 
-function Food(x, y, r, id) {
-
-  this.x = x;
-  this.y = y;
-  this.r = r;
-  this.id = id;
-
-  this.display = function() {
+  display() {
     noFill();
     stroke(255);
     ellipse(this.x, this.y, this.r, this.r);
   }
-
 }
