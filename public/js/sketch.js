6a939ab5a4fcd27c905833bd604b34db3a527c07
Luke Garrigan <lukegarrigan8@gmail.com>
2019-01-29 21:57:50 +0000

    Added average fps

--- public/js/sketch.js
+++ public/js/sketch.js
@@ -81,6 +81,9 @@ let transparentRegenImage;
 let speedOption;
 let damageOption;
 let regenOption;
+
+let totalFrames = 0;
+let sumOfFrameRates = 1;
 socket.on('foods', data => updateFoods(data, food, foodImage));
 socket.on('asteroids', data => updateAsteroids(data, asteroids, asteroidImages));
 
@@ -308,15 +311,18 @@ function drawOtherPlayers(currentPosition) {
 
 
 function displayFramesPerSecond() {
+  totalFrames++;
   let thisLoop = new Date();
   let fps = 1000 / (thisLoop - lastLoop);
   lastLoop = thisLoop;
-
+  sumOfFrameRates += fps;
   if (frameCount % 15 === 0) {
     frameRate = fps;
   }
-  text("FPS: " + floor(frameRate), width - 100, height - 50);
 
+
+  text("FPS: " + floor(frameRate), width - 100, height - 50);
+  text("AVG FPS: " + floor(sumOfFrameRates /totalFrames), width - 100, height - 25);
 }
 
 
