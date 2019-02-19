a32ebf8c361358081e757945aefa1253d73fe17b
Luke Garrigan <lukegarrigan8@gmail.com>
2019-02-19 20:41:31 +0000

    This should fix the problem where gems were spawning off the map

--- server/setupAsteroids.js
+++ server/setupAsteroids.js
@@ -3,8 +3,8 @@ module.exports = function() {
 
   let asteroids = [];
   for (let i = 0; i < config.settings.NUM_ASTEROIDS; i++) {
-    let asteroidX = Math.floor(Math.random() * (config.settings.PLAYAREA_WIDTH - 700)) + 700;
-    let asteroidY = Math.floor(Math.random() * (config.settings.PLAYAREA_HEIGHT - 600)) + 600;
+    let asteroidX = Math.floor(Math.random() * (config.settings.PLAYAREA_WIDTH - 800)) + 800;
+    let asteroidY = Math.floor(Math.random() * (config.settings.PLAYAREA_HEIGHT - 700)) + 700;
     let asteroidIndex = Math.floor(Math.random() * config.settings.NUM_ASTEROID_IMAGES);
     let asteroidRadius = Math.floor(Math.random() * 300) + 50;
 
